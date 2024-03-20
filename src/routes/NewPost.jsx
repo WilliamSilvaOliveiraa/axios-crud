16440919cba924b1b119aaa2d64a6be42162883b
William Silva <williamsilva20062005@gmail.com>
2024-03-20 10:50:45 -0300

    feat(NewPost structure)

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const NewPost = () => {
-  return <div>NewPost</div>;
+  return (
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Digite o titulo"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea
+            type="text"
+            id="body"
+            name="body"
+            placeholder="Digite o conteúdo"
+          />
+        </div>
+        <input type="submit" value="Criar Post" className="btn"></input>
+      </form>
+    </div>
+  );
 };
 
 export default NewPost;
