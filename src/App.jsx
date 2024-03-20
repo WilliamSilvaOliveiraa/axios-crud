194057e19c834e6b050d1a5def73a69c44ff6cfe
William Silva <williamsilva20062005@gmail.com>
2024-03-20 09:47:18 -0300

    style(App.css)

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <div className="Container">
+      <div className="container">
         <Outlet />
       </div>
     </div>
